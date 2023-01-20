66f28ecb61c08b9d7b0f5ff13d15c57663756566
Brandon Castro <castroyou999@gmail.com>
2023-01-19 23:41:17 -0500

    commit tree cff40a24cac011dcee0558b06bde180beb6427d1

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    # use backslash to escape syntax for special characters like . ! ?
+    self.split(/\.|\?|\!/).select{|sentence| sentence.length > 0 }.count
   end
 end
